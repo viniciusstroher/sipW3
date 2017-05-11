c1e32ba690044c138df70b4f423995299fd91e2a
Vinicius <viniciusferreirawk@gmail.com>
2017-05-11 13:25:47 -0300

    fix

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -75,7 +75,7 @@ public class SIP extends CordovaPlugin {
         try{
             Intent intent = new Intent();
             intent.setAction("org.apache.cordova.SIP.INCOMING_CALL");
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, Intent.FILL_IN_DATA);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(cordova.getActivity(), 0, intent, Intent.FILL_IN_DATA);
             mSipManager.open(mSipProfile, pendingIntent, null);
 
             Log.d("SIP","SIP CONECTADO PLUGIN: CONECTADO");
