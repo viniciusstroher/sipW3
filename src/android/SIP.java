e1a612dff6776f810ffbb618d43c003a82391618
Vinicius <viniciusferreirawk@gmail.com>
2017-05-12 14:13:02 -0300

    fix

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -121,7 +121,7 @@ public class SIP extends CordovaPlugin {
             IntentFilter filter = new IntentFilter();
             filter.addAction("org.apache.cordova.SIP.INCOMING_CALL");
             SIPReceiver callReceiver = new SIPReceiver();
-            getView().registerReceiver(callReceiver, filter);
+            cordova.getActivity().registerReceiver(callReceiver, filter);
 
         }catch(Exception e){
             Log.d("SIP","SIP PLUGIN ERROR: "+e.getMessage());
