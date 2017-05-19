d2de09800937aeb39ba5a558c70d4da1486f7fb3
Vinicius <viniciusferreirawk@gmail.com>
2017-05-19 11:38:22 -0300

    fix

--- www/module.js
+++ www/module.js
@@ -1,4 +1,5 @@
 var exec = require('cordova/exec');
+var q    = require('q');
 
 var SipW3 = {
     conectarSip:function(user,password,sip) {
@@ -9,16 +10,19 @@ var SipW3 = {
           password  : password
         };
 
-
+        var defer = Q.defer();
 
         /*RtspW3 -> nome no plugin.xml*/
         /*abrirRtsp -> metodo*/
         exec(function(suc){
             console.log(suc);
+            defer.resolve(suc);
         },function(err){
-            console.log(suc);
+            console.log(err);
+            defer.resolve(err);
         }, "SIP", "conectarSip", [params]);
         
+        return defer.promise;
     },
     
     chamar:function(address){
