dc71c631e79f8719305ab465c16764786ca0e476
Vinicius <Vinicius@w3get.com.br>
2017-06-07 15:00:33 -0300

    fix

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -81,6 +81,7 @@ public class SIP extends CordovaPlugin {
         SIP.pluginWebView = webView;
         //registra evento no module\js para envido de eventos de funcao
         Log.d("SIP","SIP PLUGIN: inicializando  navigator.SIP.recebeEvento(); ");
+        SIP.pluginWebView.loadUrl("javascript:alert('teste');");
         SIP.pluginWebView.loadUrl("javascript:navigator.SIP.recebeEvento();");
     }
 
