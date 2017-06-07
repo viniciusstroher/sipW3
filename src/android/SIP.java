fa70c52807dc355c6e0c4c71d3bcb30cb17c657f
Vinicius <Vinicius@w3get.com.br>
2017-06-07 15:45:26 -0300

    fix

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -24,6 +24,8 @@ import android.widget.RelativeLayout;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
