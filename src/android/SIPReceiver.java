3491ad504da656a93413546e2e41391227537a13
Vinicius <Vinicius@w3get.com.br>
2017-06-10 16:11:14 -0300

    fix

--- src/android/SIPReceiver.java
+++ src/android/SIPReceiver.java
@@ -71,7 +71,7 @@ public class SIPReceiver extends BroadcastReceiver {
                 notification.setLatestEventInfo(context, 
                                 "Recebendo chamada", 
                                 "Recebendo chamada!",
-                PendingIntent.getActivity(context, 0, new Intent(),android.content.Intent.FLAG_ACTIVITY_NEW_TASK));
+                PendingIntent.getActivity(context, 0, new Intent(),Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP));
                 
                 NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
                 notificationManager.notify(1, notification);
